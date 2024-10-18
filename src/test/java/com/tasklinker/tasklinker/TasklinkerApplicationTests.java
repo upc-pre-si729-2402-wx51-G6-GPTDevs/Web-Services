124383ddcd16afe8e8882182ab7320c0c312c28d
Daniel-10806 <U202211108@upc.edu.pe>
2024-10-18 17:58:53 -0500

    Chore: (TasklinkerApplicationTests.java)

--- src/test/java/com/tasklinker/tasklinker/TasklinkerApplicationTests.java
+++ src/test/java/com/tasklinker/tasklinker/TasklinkerApplicationTests.java
@@ -1,13 +1,12 @@
-package com.tasklinker.tasklinker;
+package com.example.tasklinker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TasklinkerApplicationTests {
+class TaskLinkerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
