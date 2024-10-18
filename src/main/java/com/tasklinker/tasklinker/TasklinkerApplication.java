70a1b1d5430ae957521495c6121f5c0c3e552ba5
Franco Durand <francoduve.25@gmail.com>
2024-10-18 17:09:53 -0500

    first commit

---
+++ src/main/java/com/tasklinker/tasklinker/TasklinkerApplication.java
@@ -0,0 +1,13 @@
+package com.tasklinker.tasklinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasklinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TasklinkerApplication.class, args);
+	}
+
+}
