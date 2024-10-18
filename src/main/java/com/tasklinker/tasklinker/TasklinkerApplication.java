43b6397dc378b7a7c5afdbefb18ece18fb254910
Daniel-10806 <U202211108@upc.edu.pe>
2024-10-18 17:57:30 -0500

    Chore: (TasklinkerApplication.java)

--- src/main/java/com/tasklinker/tasklinker/TasklinkerApplication.java
+++ src/main/java/com/tasklinker/tasklinker/TasklinkerApplication.java
@@ -1,13 +1,11 @@
-package com.tasklinker.tasklinker;
+package com.example.tasklinker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TasklinkerApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TasklinkerApplication.class, args);
-	}
-
+public class TaskLinkerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskLinkerApplication.class, args);
+    }
 }
