089fee908583f6369ca0325a42206e596ea155a5
Daniel-10806 <U202211108@upc.edu.pe>
2024-11-16 00:03:41 -0500

    Delete src/main/java/com/tasklinker/tasklinker/repository directory

--- src/main/java/com/tasklinker/tasklinker/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.tasklinker.repository;
-
-import com.example.tasklinker.model.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
