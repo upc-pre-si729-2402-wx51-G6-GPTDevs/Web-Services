8cb4689fa758dc1703be429d36bb3d5b04e3ee8d
Daniel-10806 <U202211108@upc.edu.pe>
2024-11-16 18:09:09 -0500

    chore(application): update imports

--- src/main/java/com/tasklinker/tasklinker/domain/services/NotificationCommandService.java
+++ src/main/java/com/tasklinker/tasklinker/domain/services/NotificationCommandService.java
@@ -1,6 +1,6 @@
 package com.tasklinker.tasklinker.domain.services;
 
-import com.tasklinker.tasklinker.Application.internal.command.SendNotificationCommand;
+import com.tasklinker.tasklinker.application.internal.command.SendNotificationCommand;
 
 public interface NotificationCommandService {
     void handle(SendNotificationCommand command);
