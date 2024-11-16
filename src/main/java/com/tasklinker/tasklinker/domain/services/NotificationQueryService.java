443faecbf5490819d44d335facab5ba12a3e3b79
Daniel-10806 <U202211108@upc.edu.pe>
2024-11-16 18:09:31 -0500

    chore(application): update imports

--- src/main/java/com/tasklinker/tasklinker/domain/services/NotificationQueryService.java
+++ src/main/java/com/tasklinker/tasklinker/domain/services/NotificationQueryService.java
@@ -1,6 +1,6 @@
 package com.tasklinker.tasklinker.domain.services;
 
-import com.tasklinker.tasklinker.Application.internal.query.GetNotificationsQuery;
+import com.tasklinker.tasklinker.application.internal.query.GetNotificationsQuery;
 import com.tasklinker.tasklinker.domain.model.entities.Notification;
 
 import java.util.List;
