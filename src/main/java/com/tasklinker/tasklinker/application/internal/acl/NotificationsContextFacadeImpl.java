51366d4100824cfa239cc8db6f45524d4785967e
Daniel-10806 <U202211108@upc.edu.pe>
2024-11-16 17:37:27 -0500

    feat(application): add NotificationsContextFacadeImpl

---
+++ src/main/java/com/tasklinker/tasklinker/application/internal/acl/NotificationsContextFacadeImpl.java
@@ -0,0 +1,35 @@
+package com.tasklinker.tasklinker.Application.internal.acl;
+
+import com.tasklinker.tasklinker.domain.model.entities.Notification;
+import org.springframework.stereotype.Service;
+import com.tasklinker.tasklinker.Application.internal.command.SendNotificationCommand;
+import com.tasklinker.tasklinker.Application.internal.query.GetNotificationsQuery;
+import com.tasklinker.tasklinker.Application.internal.command.SendNotificationHandler;
+import com.tasklinker.tasklinker.Application.internal.query.GetNotificationsHandler;
+import com.tasklinker.tasklinker.interfaces.rest.acl.NotificationContextFacade;
+
+import java.util.List;
+
+@Service
+public class NotificationsContextFacadeImpl implements NotificationContextFacade {
+
+    private final SendNotificationHandler sendNotificationHandler;
+    private final GetNotificationsHandler getNotificationsHandler;
+
+    public NotificationsContextFacadeImpl(SendNotificationHandler sendNotificationHandler,
+                                          GetNotificationsHandler getNotificationsHandler) {
+        this.sendNotificationHandler = sendNotificationHandler;
+        this.getNotificationsHandler = getNotificationsHandler;
+    }
+
+    @Override
+    public void sendNotification(String message, String recipient) {
+        var command = new SendNotificationCommand(message, recipient);
+        sendNotificationHandler.handle(command);
+    }
+
+    @Override
+    public List<Notification> fetchNotifications() {
+        return getNotificationsHandler.handle(new GetNotificationsQuery());
+    }
+}
