f1ec2f88a8d0d1c7b36a74c214802272aca1d24f
Daniel-10806 <U202211108@upc.edu.pe>
2024-11-16 17:24:07 -0500

    chore(test): update code and add imports

--- src/test/java/com/tasklinker/tasklinker/TasklinkerApplicationTests.java
+++ src/test/java/com/tasklinker/tasklinker/TasklinkerApplicationTests.java
@@ -1,12 +1,31 @@
-package com.example.tasklinker;
+package com.example.webservicenotificationsbc;
 
-import org.junit.jupiter.api.Test;
+import com.example.webservicenotificationsbc.Application.internal.command.SendNotificationCommand;
+import com.example.webservicenotificationsbc.Application.internal.command.SendNotificationHandler;
+import com.example.webservicenotificationsbc.domain.model.entities.Notification;
+import com.example.webservicenotificationsbc.infrastructure.persistence.jpa.repositories.NotificationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class TaskLinkerApplicationTests {
+public class NotificationServiceTest {
+
+    @Autowired
+    private NotificationRepository repository;
+
+    @Autowired
+    private SendNotificationHandler handler;
 
     @Test
-    void contextLoads() {
+    public void shouldSendNotification() {
+        handler.handle(new SendNotificationCommand("Hello", "user@example.com"));
+
+        Notification notification = repository.findAll().getFirst();
+
+        assertEquals("Hello", notification.getMessage());
+        assertEquals("user@example.com", notification.getRecipient());
+        assertEquals("TaskLinker Context - Facilitando trabajos por horas en Perú", notification.getContext());
     }
 }
